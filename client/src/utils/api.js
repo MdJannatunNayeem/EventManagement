dd3d174ad7c4e1f1c81f47f3d7a18fe7427e2fc7
Nayeem <nayeemae19@gmail.com>
2025-09-07 06:54:45 +0600

    set the backend url which deployed

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:5050'; // backend URL
+const API_URL = 'https://eventmanagement-qv0d.onrender.com'; // backend URL
 
 export const api = axios.create({
     baseURL: API_URL + '/api'
@@ -10,5 +10,5 @@ export function setAuthToken(token) {
     if (token) api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     else delete api.defaults.headers.common['Authorization'];
 }
-
-export const IMAGE_API= "http://localhost:5050/upload-file/";
+pload
+export const IMAGE_API= "https://eventmanagement-qv0d.onrender.com/";
